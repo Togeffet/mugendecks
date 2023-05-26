71fec0c3632572a9fb57ced7fff5b772aaaea42b
Franklin Fanelli <togeffet@gmail.com>
2023-05-25 23:15:19 -0500

    Add search bar and simple js and css stuff

--- index.php
+++ index.php
@@ -1,28 +1,79 @@
 <?php
-  session_start();
-  require_once('dictionary.php');
+session_start();
+require_once('dictionary.php');
 
-  // ini_set('display_errors', 1);
-  // ini_set('display_startup_errors', 1);
-  // error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-  require('scripts.php');
-  require '../vendor/autoload.php';
+require('scripts.php');
+require '../vendor/autoload.php';
 
-  $dexter = new Dexter();
-  $random_int = rand(0, 211387);
+if (isset($_GET['search_term']) && $_GET['search_term'] != "") {
+  $search_term = $_GET['search_term'];
+  $search_term = "%${search_term}%";
 
-  if ($stmt = mysqli_prepare($conn, 
-      "SELECT KJ.kanji, KN.kana, G.meaning, G.applies_to_kanji, G.applies_to_kana FROM (SELECT * FROM jmdict_vocab ORDER BY RAND() LIMIT 1) V
-        LEFT JOIN jmdict_kanji KJ ON KJ.vocab_id = V.vocab_id
-        LEFT JOIN jmdict_kana KN ON KN.vocab_id = V.vocab_id AND (KN.applies_to_kanji = 0 OR KN.applies_to_kanji = KJ.kanji_sub_id)
-        LEFT JOIN jmdict_glossary G ON G.vocab_id = V.vocab_id")) {
+  $stmt = mysqli_prepare(
+    $conn,
+    "SELECT DISTINCT V.vocab_id, KJ.kanji, KN.kana, G.meaning, G.applies_to_kanji, G.applies_to_kana FROM
+            (SELECT vocab_id FROM jmdict_kanji KJ WHERE KJ.kanji LIKE ?
+              UNION
+             SELECT vocab_id FROM jmdict_kana KN WHERE KN.kana LIKE ?
+              UNION
+             SELECT vocab_id FROM jmdict_glossary G WHERE G.meaning LIKE ?) V
+          LEFT JOIN jmdict_kanji KJ ON KJ.vocab_id = V.vocab_id
+          LEFT JOIN jmdict_kana KN ON KN.vocab_id = V.vocab_id AND (KN.applies_to_kanji = 0 OR KN.applies_to_kanji = KJ.kanji_sub_id)
+          LEFT JOIN jmdict_glossary G ON G.vocab_id = V.vocab_id
+          GROUP BY V.vocab_id
+          LIMIT 50"
+  );
+  mysqli_stmt_bind_param($stmt, "sss", $search_term, $search_term, $search_term);
+} else {
+  $stmt = mysqli_prepare(
+    $conn,
+    "SELECT V.vocab_id, KJ.kanji, KN.kana, G.meaning, G.applies_to_kanji, G.applies_to_kana FROM (SELECT * FROM jmdict_vocab ORDER BY RAND() LIMIT 1) V
+          LEFT JOIN jmdict_kanji KJ ON KJ.vocab_id = V.vocab_id
+          LEFT JOIN jmdict_kana KN ON KN.vocab_id = V.vocab_id AND (KN.applies_to_kanji = 0 OR KN.applies_to_kanji = KJ.kanji_sub_id)
+          LEFT JOIN jmdict_glossary G ON G.vocab_id = V.vocab_id"
+  );
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php
+  echoHeaderMeta($css_version);
+  ?>
+</head>
+<body>
+  <div class="mainspace">
+    <div class="content">
+      <div><a href="/">無限デックス</a></div>
+      <div>Test out the database with a search or just refresh the page for a random vocab word! Right now it's just returning simple data (e.g. only one definition and no tags yet but I'm working on that query now) but it'll search kanji, kana, and meanings for your search term so try it out!</div>
+      <form action="/" method="get">
+        <input type="text" name="search_term" placeholder="Search" />
+        <input type="submit" value="Search" />
+      </form>
 
-    if (mysqli_stmt_execute($stmt)) {
-      $result = $stmt->get_result();
-      while ($row = $result->fetch_assoc()) {
-        echo print_r($row) . '<br>';
-      }
-      
-    }
-  }
+      <table>
+        <tr><td>Vocab ID</td><td>Kanji</td><td>Kana</td><td>Meaning</td><td>When read as (kanji)</td><td>When read as (kana)</td></tr>
+        <?php
+        if (mysqli_stmt_execute($stmt)) {
+          $result = $stmt->get_result();
+          while ($row = $result->fetch_assoc()) {
+            $vocab_id = $row['vocab_id'];
+            $kanji = $row['kanji'];
+            $kana = $row['kana'];
+            $meaning = $row['meaning'];
+            $applies_to_kanji = $row['applies_to_kanji'];
+            $applies_to_kana = $row['applies_to_kana'];
+            echo "<tr><td>$vocab_id</td><td>$kanji</td><td>$kana</td><td>$meaning</td><td>$applies_to_kanji</td><td>$applies_to_kana</td></tr>\n";
+          }
+        }
+        ?>
+      </table>
+    </div>
+  </div>
+</body>
+<?php echoDefaultJSLink($js_version) ?>
+</html>
--- scripts.php
+++ scripts.php
@@ -2,8 +2,50 @@
 session_start();
 require '/unimportant.php'; // Important file outside webserver
 
+$js_version = 1;
+$css_version = 1;
+
 // Database connection
 $conn = mysqli_connect($dblocation, $dbuser, $dbpass, "jmdict");
 $conn->set_charset("utf8mb4");
 
+function echoHeaderMeta($css_version, $allow_crawl = true, $title = "無限デックス", $description = "Learn Japanese using scripts from your favorite anime, novels, movies, and more!", 
+    $social_title = "無限デックス", $social_description = "Learn Japanese using scripts from your favorite anime, novels, movies, and more!") {
+  echo '<style>html { background-color: #212121; color: #FAFAFA; } body { opacity: 0 } }</style>
+  <base href="/" />
+  <meta charset="utf-8" />
+  <meta name="google" content="notranslate" />'.
+  (!$allow_crawl ? '<meta name="robots" content="noindex">' : '');
+  echo '<title>出来るか? - '.$title.'</title>
+  <meta name="type" content="website" />
+  <meta name="image" content="apple-touch-icon.png" />
+  <meta name="url" content="https://mugendecks.com" />
+  <meta name="description" content="'.$description.'" />
+
+  <meta property="og:title" content="'.$social_title.'" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="apple-touch-icon.png" />
+  <meta property="og:url" content="https://mugendecks.com" />
+  <meta property="og:description" content="'.$social_description.'" />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500;900&family=Noto+Serif+JP:wght@300;500;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@300;500;900&display=swap" rel="stylesheet">
+
+  <link rel="preload" href="https://fonts.googleapis.com/icon?family=Material+Icons&display=swap" as="style">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons&display=swap">';
+
+  echo '<link rel="preload" href="styles/styles.css?'.$css_version.'" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="styles.css?'.$css_version.'"></noscript>';
+}
+
+function echoDefaultJSLink($js_version) {
+  echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>\n
+        <script src='/js/js.js?$js_version' type='text/javascript'></script>";
+}
 ?>
